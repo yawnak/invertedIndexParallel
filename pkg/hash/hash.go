5ae0fb5e43ca9f07fdf1363c6e021a5df17aba0e
@pudgebooster <ukraniandollar@gmail.com>
2022-10-27 19:24:42 +0300

    fix: change HashString to use library hash

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -1,9 +1,9 @@
 package hash
 
-func HashString(s string) int64 {
-	var hash int64 = 7
-	for char := range s {
-		hash = hash*31 + int64(char)
-	}
-	return hash
+import "hash/fnv"
+
+func HashString(s string) uint64 {
+	h := fnv.New64()
+	h.Write([]byte(s))
+	return h.Sum64()
 }
