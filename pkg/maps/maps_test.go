2e7b3b3ceb5cae14bf85f19e6fc37b1a1bdbca4d
@pudgebooster <ukraniandollar@gmail.com>
2022-11-02 22:38:44 +0200

    test: TestMapper

--- pkg/maps/maps_test.go
+++ pkg/maps/maps_test.go
@@ -1,6 +1,7 @@
 package maps
 
 import (
+	"os"
 	"testing"
 
 	"github.com/asstronom/invertedIndexParallel/pkg/domain"
@@ -8,7 +9,17 @@ import (
 
 func TestMap(t *testing.T) {
 	m := Mapper{}
-	out := make(chan domain.Token)
-	
-	m.Map([[]struct{int64 *os.File}{0, nil}], out)
+	f1, err := os.Open("test_data/0_2.txt")
+	f2, err := os.Open("test_data/1_3.txt")
+	f3, err := os.Open("test_data/2_3.txt")
+	f4, err := os.Open("test_data/3_4.txt")
+	if err != nil {
+		t.Errorf("error opening file: %s", err)
+	}
+	sl := make([]domain.FileToken, 4)
+	sl[0] = domain.FileToken{DocID: 0, File: f1}
+	sl[1] = domain.FileToken{DocID: 1, File: f2}
+	sl[2] = domain.FileToken{DocID: 2, File: f3}
+	sl[3] = domain.FileToken{DocID: 3, File: f4}
+	m.Map(sl)
 }
