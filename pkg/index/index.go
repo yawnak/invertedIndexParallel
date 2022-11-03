e92e60736fd6968c813f1fd8774c474dc12fc883
asstronom <ukraniandollar@gmail.com>
2022-11-03 16:42:01 +0200

    feature: add Index

--- pkg/index/index.go
+++ pkg/index/index.go
@@ -1,4 +1,17 @@
 package index
 
+import "github.com/asstronom/invertedIndexParallel/pkg/maps"
+
+type Architect struct {
+}
+
+type Index struct {
+	mappers []maps.Mapper
+}
+
+//n - number of mappers, m - numbers of reducers
+func NewIndex(n int, m int) *Index {
+	return &Index{mappers: make([]maps.Mapper, n)}
+}
 
 
