a64490f581038cc5ee47fecb55622bed10d41828
asstronom <ukraniandollar@gmail.com>
2022-11-05 14:06:43 +0200

    feature: stripNonLetters

--- pkg/maps/maps.go
+++ pkg/maps/maps.go
@@ -3,6 +3,7 @@ package maps
 import (
 	"bufio"
 	"fmt"
+	"unicode"
 
 	"github.com/asstronom/invertedIndexParallel/pkg/dict"
 	"github.com/asstronom/invertedIndexParallel/pkg/domain"
@@ -20,6 +21,25 @@ func linSearch(docid int64, tokens []domain.WordToken) int {
 	return -1
 }
 
+func stripNonLetters(s string) string {
+	word := []rune(s)
+	startidx := 0
+	endidx := 0
+	for i := 0; i < len(word); i++ {
+		if unicode.IsLetter(word[i]) {
+			startidx = i
+			break
+		}
+	}
+	for i := len(word) - 1; i >= 0; i-- {
+		if unicode.IsLetter(word[i]) {
+			endidx = i + 1
+			break
+		}
+	}
+	return string(word[startidx:endidx])
+}
+
 func (m *Mapper) Map(filetokens []domain.FileToken, out chan<- []domain.WordToken) {
 	d := dict.NewDictionary(50)
 	for _, filetoken := range filetokens {
@@ -27,6 +47,10 @@ func (m *Mapper) Map(filetokens []domain.FileToken, out chan<- []domain.WordToke
 		scanner.Split(bufio.ScanWords)
 		for scanner.Scan() {
 			word := scanner.Bytes()
+			word = []byte(stripNonLetters(string(word)))
+			if len(word) == 0 {
+				continue
+			}
 			tkn, ok := d.Get(string(word))
 			if !ok {
 				tkn = []domain.WordToken{{Docid: filetoken.DocID, Term: string(word), Count: 1}}
