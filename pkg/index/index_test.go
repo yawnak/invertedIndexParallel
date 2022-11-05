e8a769d399e95eea77ef6a95d59aa8f1260b881c
asstronom <ukraniandollar@gmail.com>
2022-11-05 13:10:57 +0200

    feature: TestIndex

---
+++ pkg/index/index_test.go
@@ -0,0 +1,19 @@
+package index
+
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	f1, err := os.Open("test_data/0_2.txt")
+	f2, err := os.Open("test_data/1_3.txt")
+	f3, err := os.Open("test_data/2_3.txt")
+	f4, err := os.Open("test_data/3_4.txt")
+	if err != nil {
+		t.Errorf("error opening file: %s", err)
+	}
+	idx := NewIndex(2, 0)
+	idx.IndexDocs([]io.Reader{f1, f2, f3, f4})
+}
