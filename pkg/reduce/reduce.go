d14b38f95e14921237938bda6dc07f2ca06be62a
asstronom <ukraniandollar@gmail.com>
2022-11-05 13:11:56 +0200

    feature: add Reducer

--- pkg/reduce/reduce.go
+++ pkg/reduce/reduce.go
@@ -1,5 +1,18 @@
 package reduce
 
+import (
+	"github.com/asstronom/invertedIndexParallel/pkg/dict"
+	"github.com/asstronom/invertedIndexParallel/pkg/domain"
+)
+
 type Reducer struct {
+	d *dict.Dictionary
 }
 
+func NewReducer() *Reducer {
+	return &Reducer{d: dict.NewDictionary(50)}
+}
+
+func (r *Reducer) Reduce(chan []domain.WordToken) {
+	
+}
